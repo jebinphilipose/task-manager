44f3e62ec7289a242cbf2ec7fcf3690b7b0ced61
Jebin Philipose <jebinphilip24@gmail.com>
2020-10-23 17:50:47 +0530

    Add APIs for pausing, resuming and cancelling Tasks

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import upload_csv
+from .views import upload_csv, pause_task, resume_task, cancel_task
 
 
 urlpatterns = [
+    path('task/<int:task_id>/pause/', pause_task, name="pause_task"),
+    path('task/<int:task_id>/resume/', resume_task, name="resume_task"),
+    path('task/<int:task_id>/cancel/', cancel_task, name="cancel_task"),
     path('upload/', upload_csv, name="upload_csv"),
 ]
--- api/views.py
+++ api/views.py
@@ -6,10 +6,34 @@ from .tasks import import_csv_data, delete_task
 
 # Create your views here.
 @csrf_exempt
+def pause_task(request, task_id):
+    if request.method == 'PATCH':
+        task = Task.objects.get(id=task_id)
+        task.status = Task.TaskStatus.PAUSED
+        task.save()
+        return JsonResponse({'response': f'Task with id: {task.id} paused'})
+
+@csrf_exempt
+def resume_task(request, task_id):
+    if request.method == 'PATCH':
+        task = Task.objects.get(id=task_id)
+        task.status = Task.TaskStatus.PROCESSING
+        task.save()
+        return JsonResponse({'response': f'Task with id: {task.id} resumed'})
+
+@csrf_exempt
+def cancel_task(request, task_id):
+    if request.method == 'PATCH':
+        task = Task.objects.get(id=task_id)
+        task.status = Task.TaskStatus.CANCELLED
+        task.save()
+        return JsonResponse({'response': f'Task with id: {task.id} cancelled'})
+
+@csrf_exempt
 def upload_csv(request):
     if request.method == 'POST':
         # Create a new Task
         task = Task.objects.create(type='import_csv_data')
         # Run Celery task
         import_csv_data.apply_async((os.path.dirname(os.path.dirname(__file__)) + '/users.csv', task.id), link=delete_task.s(task.id))
-        return JsonResponse({'response': f'Task with id: {task.id} started'})
+        return JsonResponse({'response': f'CSV upload with Task id: {task.id} started'}, status=201)
