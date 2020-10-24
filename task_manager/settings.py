921a5cedc30a40c944b61d75973cfd3a5508470e
Jebin Philipose <jebinphilip24@gmail.com>
2020-10-25 01:02:45 +0530

    Dockerize application

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '',
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
